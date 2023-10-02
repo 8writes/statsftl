8e3eaa2e41598f53261594fe88f8a3efce30d877
Chisom <mailemmanuel00@gmail.com>
2023-10-02 08:43:20 +0100

    Update page.js

--- src/components/header/page.js
+++ src/components/header/page.js
@@ -5,10 +5,7 @@ export default function Header() {
     <>
       <div className={styles.header__wrapper}>
         <div className={styles.header}>
-          <h2>ZTATS</h2>
-        </div>
-        <div>
-          <Link href="https://appztats.brimble.app/signup" target="_blank">Signup</Link> /<Link href="https://appztats.brimble.app/login" target="_blank">Login</Link>
+          <h2>STATSFTL</h2>
         </div>
       </div>
     </>
